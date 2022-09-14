703c1480fe8525c55bc6ef19fa9a8ac8db4ff88b
Ruhge <ruhage26@gmail.com>
2022-09-14 12:19:28 +1000

    hw1.6.2

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-
         /// Задание 1
         System.out.println("Задание 1");
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
+        for (int year = 1906; year <= 2096; year++) {
+            if (year % 4 ==0 && year % 100 != 0 || year %400 ==0) {
+                System.out.println(year + " год является високосным");
+            }
         }
 
         /// Задание 2
         System.out.println("Задание 2");
-        for (int i = 10; i >= 1; i--) {
+        for (int i = 7; i <= 98; i += 7) {
             System.out.println(i);
         }
 
         /// Задание 3
         System.out.println("Задание 3");
-        for (int i = 0; i < 17; i += 2) {
-            System.out.println(i);
-        }
-
-        /// Задание 4
-        System.out.println("Задание 4");
-        for (int i = 10; i >= -10; i--) {
-            System.out.println(i);
+        int number = 1;
+        System.out.println(number);
+        for (int i = 0; i < 9; i++) {
+            number = number * 2;
+                System.out.println(number);
         }
     }
 }
